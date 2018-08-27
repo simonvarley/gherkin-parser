f3cd3a4a82e1fbb14bd8445e099ea1707e61aa74
Simon Varley <simonvarley101@gmail.com>
2018-08-26 20:29:46 -0400

    Initial JUnit test to navigate a gherkin feature file.

---
+++ gherkin-parser/src/test/java/com/simonvarley/cucumber/GerkTest2.java
@@ -0,0 +1,110 @@
+package com.simonvarley.cucumber;
+
+import static org.junit.Assert.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import gherkin.AstBuilder;
+import gherkin.Parser;
+import gherkin.TokenMatcher;
+import gherkin.ast.Feature;
+import gherkin.ast.GherkinDocument;
+import gherkin.ast.Scenario;
+import gherkin.ast.ScenarioDefinition;
+import gherkin.ast.Step;
+import gherkin.ast.Tag;
+import gherkin.pickles.Pickle;
+//import gherkin.pickles.Pickle;
+import gherkin.util.FixJava;
+
+import org.junit.Test;
+
+public class GerkTest2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test1() throws UnsupportedEncodingException, FileNotFoundException, RuntimeException {
+		String path = "C:\\Projects\\Testdata\\gerktest.feature";
+		String gherkin = FixJava.readReader(new InputStreamReader(new FileInputStream(path), "UTF-8"));
+
+		Parser<GherkinDocument> parser = new Parser<>(new AstBuilder());
+		TokenMatcher matcher = new TokenMatcher();
+
+		GherkinDocument d1 = parser.parse("Feature: 1", matcher);
+		GherkinDocument d2 = parser.parse("Feature: 2", matcher);
+		GherkinDocument d3 = parser.parse(gherkin, matcher);
+
+		assertEquals("1", d1.getFeature().getName());
+		assertEquals("2", d2.getFeature().getName());
+		assertEquals("Withdraw Cash", d3.getFeature().getName());
+
+	}
+
+	@Test
+	public void test2() throws UnsupportedEncodingException, FileNotFoundException, RuntimeException {
+		String path = "C:\\Projects\\Testdata\\gerktest.feature";
+		String gherkin = FixJava.readReader(new InputStreamReader(new FileInputStream(path), "UTF-8"));
+
+		Parser<GherkinDocument> parser = new Parser<>(new AstBuilder());
+		TokenMatcher matcher = new TokenMatcher();
+
+		GherkinDocument d3 = parser.parse(gherkin, matcher);
+
+		assertEquals("Withdraw Cash", d3.getFeature().getName());
+
+		Feature feature = d3.getFeature();
+		String description = feature.getDescription();
+		assertEquals( "  This feature allows an account holder to withdraw\n  cash from an ATM. ",
+				description );
+
+		List<Tag> tags = feature.getTags();
+		assertEquals( 1, tags.size() );
+		assertEquals( "@F2421", tags.get(0).getName());
+
+
+		List<ScenarioDefinition> parts = feature.getChildren();
+		assertEquals( 3, parts.size() );
+
+		for( ScenarioDefinition sd : parts ) {
+			// Scenario, Background, Scenario Outline, etc.
+			System.out.println( "Name: " + sd.getName() );
+			System.out.println( "Description: " + sd.getDescription() );
+			System.out.println( "Keyword: " + sd.getKeyword() );
+			if (sd.getKeyword().equalsIgnoreCase("Scenario")) {
+				Scenario s = (Scenario)sd;
+				List<Tag> sTags = s.getTags();
+				for( Tag tag : sTags ) {
+					System.out.println( "Scenario tags: " + tag.getName() );
+				}
+			}
+
+			List<Step> steps = sd.getSteps();
+			for( Step s : steps ) {
+				// Given, When, Then, And, etc
+				System.out.println( "Steps Keyword: " + s.getKeyword() );
+			}
+		}
+	}
+
+	@Test
+	public void test3() throws UnsupportedEncodingException, FileNotFoundException, RuntimeException {
+		// Java
+
+		String path = "C:\\Projects\\Testdata\\gerktest.feature";
+		String gherkin = FixJava.readReader(new InputStreamReader(new FileInputStream(path), "UTF-8"));
+
+		Parser<GherkinDocument> parser = new Parser<>(new AstBuilder());
+		GherkinDocument gherkinDocument = parser.parse(gherkin);
+		List<Pickle> pickles = new Compiler().compile(gherkinDocument, "C:\\Projects\\Testdata\\gerktest.feature");
+		assertEquals( 10, pickles.size() );
+	}
+
+}
