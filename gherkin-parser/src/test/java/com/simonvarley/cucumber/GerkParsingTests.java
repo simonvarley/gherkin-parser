75c9d03ca61481aab569e4e4be88739005a22113
Simon Varley <simonvarley101@gmail.com>
2021-06-04 20:44:43 -0400

    replace file reader lib, and tdd GherkinParser class

--- gherkin-parser/src/test/java/com/simonvarley/cucumber/GerkParsingTests.java
+++ gherkin-parser/src/test/java/com/simonvarley/cucumber/GerkParsingTests.java
@@ -1,32 +1,27 @@
 package com.simonvarley.cucumber;
 
-import static org.junit.Assert.*;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.List;
-
 import gherkin.AstBuilder;
 import gherkin.Parser;
 import gherkin.TokenMatcher;
-import gherkin.ast.Feature;
-import gherkin.ast.GherkinDocument;
-import gherkin.ast.Scenario;
-import gherkin.ast.ScenarioDefinition;
-import gherkin.ast.Step;
-import gherkin.ast.Tag;
-import gherkin.util.FixJava;
-
+import gherkin.ast.*;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class GerkParsingTests {
 
 	@Test
-	public void basicAstParsingTest() throws UnsupportedEncodingException, FileNotFoundException, RuntimeException {
+	public void basicAstParsingTest() throws IOException, RuntimeException {
 		String path = "src\\test\\resources\\gerktest.feature";
-		String gherkin = FixJava.readReader(new InputStreamReader(new FileInputStream(path), "UTF-8"));
+		final String gherkin = FileUtils.readFileToString( new File( path ), StandardCharsets.UTF_8 );
 
 		Parser<GherkinDocument> parser = new Parser<>(new AstBuilder());
 		TokenMatcher matcher = new TokenMatcher();
@@ -42,9 +37,9 @@ public class GerkParsingTests {
 	}
 
 	@Test
-	public void fullFeatureAndScenarioTags() throws UnsupportedEncodingException, FileNotFoundException, RuntimeException {
+	public void fullFeatureAndScenarioTags() throws IOException, RuntimeException {
 		String path = "src\\test\\resources\\gerktest.feature";
-		String gherkin = FixJava.readReader(new InputStreamReader(new FileInputStream(path), "UTF-8"));
+		final String gherkin = FileUtils.readFileToString( new File( path ), StandardCharsets.UTF_8 );
 
 		Parser<GherkinDocument> parser = new Parser<>(new AstBuilder());
 		TokenMatcher matcher = new TokenMatcher();
@@ -85,5 +80,14 @@ public class GerkParsingTests {
 				System.out.println( "Steps Keyword: " + s.getKeyword() );
 			}
 		}
+	}
+
+	@Test
+	public void startRefactoringTheCode() {
+		String singleFeatureFile = "src/test/resources/gerktest.feature";
+
+		GherkinParser bdd = new GherkinParser( singleFeatureFile );
+
+		assertThat( bdd.getName(), is("Withdraw Cash") );
 	}
 }
